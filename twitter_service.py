901738a0ff54723ebf541ca286f118511c2eecb5
Jebin Einstein <jebineinstein@gmail.com>
2025-09-06 22:40:59 +0530

    new ch

--- twitter_service.py
+++ twitter_service.py
@@ -26,7 +26,7 @@ class TwitterService(TwitterProp):
 	def _get_actions(self):
 		actions = self.twitter_config["actions"].split(",")
 		random.shuffle(actions)
-		return actions
+		return [x for a in actions[:-1] for x in (a, "post")] + [actions[-1]] # adding post alternatively
 
 	def did_login(self):
 		try:
