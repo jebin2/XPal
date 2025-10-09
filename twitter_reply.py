1423ac00d6638033b07d51514b8eace89f4dd42c
Jebin Einstein <jebineinstein@gmail.com>
2025-10-09 21:40:30 +0530

    limit ch

--- twitter_reply.py
+++ twitter_reply.py
@@ -65,7 +65,7 @@ class TwitterReply(TwitterProp):
 			max_itr -= 1
 			logger_config.info(f'{self.twitter_config["wait_second"]} sec scroll')
 			x_utils.simulate_human_scroll(self.page, self.twitter_config["wait_second"])
-			if count > self.twitter_config["reply_count"] or max_itr < 0:
+			if count >= self.twitter_config["reply_count"] or max_itr < 0:
 				break
 
 			logger_config.info(f"Getting new post, old_post:: {old_post}")
